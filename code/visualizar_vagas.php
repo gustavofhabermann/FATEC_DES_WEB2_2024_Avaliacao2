676bfeecedcb8650184428db357560690939bfed
gustavofhabermann <160959136+gustavofhabermann@users.noreply.github.com>
2024-11-11 20:27:08 -0300

    Update visualizar_vagas.php

--- code/visualizar_vagas.php
+++ code/visualizar_vagas.php
@@ -18,7 +18,7 @@ $vagas = $cadastro->Listar_Vagas();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/visualizar.css">
+    <link rel="stylesheet" href="../css/visualizar.css">
 </head>
 <body>
     
